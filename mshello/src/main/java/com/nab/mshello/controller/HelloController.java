008913e0b589fb1b15f7ab6a8a3a5c789765713f
Nicolas Bloj <nicolasbloj@gmail.com>
2017-12-12 17:38:48 -0300

    Spring-boot Property - Environment, @Value, ...

--- mshello/src/main/java/com/nab/mshello/controller/HelloController.java
+++ mshello/src/main/java/com/nab/mshello/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.nab.mshello.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,14 +15,26 @@ public class HelloController {
 
 	@Autowired
 	HelloService helloService;
+
+	@Autowired
+	private Environment environment;
+	
+	@Value("${server.port}")
+	private String port;
 	
 	@RequestMapping
-	@HystrixCommand(fallbackMethod="helloFallBack")
+	@HystrixCommand(fallbackMethod = "helloFallBack")
 	public String hello() {
 		return helloService.hello();
 	}
-	
+
 	public String helloFallBack() {
 		return "Hello World ... CIRCUIT OPEN ... Fall back method";
+	}
+
+	@RequestMapping("/getPort")
+	public String getPort() {
+		//return environment.getProperty("server.port");
+		return   port;
 	}
 }
